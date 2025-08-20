fa9d5883f6291d23a7ce3339665481eb02be8dc0
acinis <72325650+acinis@users.noreply.github.com>
2025-08-20 22:08:09 +0200

    Use version numbers in sample window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SDL3/SDL.h>
 
+#include "Version.hpp"
+
 // SDL3 smoke-test app
 
 int main()
@@ -18,6 +20,11 @@ int main()
             std::cerr << "Cannot create window: " << SDL_GetError() << "\n";
         }
         else {
+            std::string window_title = std::format("Hello from v{}", rh::Version::current());
+            if (! SDL_SetWindowTitle(window, window_title.c_str())) {
+                std::cerr << "Cannot set window title: " << SDL_GetError() << "\n";
+            }
+
             screen_surface = SDL_GetWindowSurface(window);
 
             SDL_FillSurfaceRect(screen_surface, nullptr, SDL_MapSurfaceRGB(screen_surface, 0x6D, 0xD6, 0x61));
